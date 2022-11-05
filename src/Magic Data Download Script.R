f3541e99683c7e727a645ff7ff239b79cf1981f0
samoox502 <66544558+samoox502@users.noreply.github.com>
2022-11-05 11:33:08 -0500

    Accidentally undid the data script and had to redo it.

--- src/Magic Data Download Script.R
+++ src/Magic Data Download Script.R
@@ -10,7 +10,7 @@ library(data.table)
 library(magick)
 
 # Set working directory
-setwd("~/R/Magic the Virgining")
+setwd("~/R/Magic the Virgining/Magic-the-Gathering")
 
 # Download bulk card data
 cards = 
@@ -23,196 +23,74 @@ cards =
 cards$set = 
   toupper(cards$set) 
 
-#only want to use english cards
+#only want to use english cards, removing columns that contain lists that probably aren't too useful, and also unnesting the relevant columns
 english_cards = 
   cards %>% 
   filter(
     lang == 'en'
-  )
-
-df = english_cards %>%
-  select(
-    name,
-    rarity,
-    oracle_text,
-    set,
-    released_at,
-    large
-  )
-
-fwrite(df, 'magic-cards-input.csv', row.names = F)
-
-colnames(english_cards)
-
-english_cards = english_cards %>% 
+  ) %>%
+  select(!c(color_identity,
+            all_parts,
+            preview,
+            produced_mana,
+            multiverse_ids,
+            artist_ids,
+            finishes,
+            color_indicator,
+            frame_effects,
+            card_faces,
+            promo_types)) %>% 
   unnest(
-    c(legalities, image_uris, prices, related_uris), 
+    c(legalities,
+      image_uris,
+      prices,
+      related_uris), 
     keep_empty = TRUE
-)
+  )
 
+#Time to unnest all of the colors, keywords, and anything else we need
+card_colors = english_cards %>%
+  unnest(
+    colors,
+    keep_empty = T
+  ) %>%
+  group_by(id) %>%
+  summarise(colors_pulled = paste0(colors, collapse = ','))
 
+english_cards$colors = card_colors$colors_pulled
 
-dim(english_cards)
-english_cards
-head(english_cards)
+card_keywords = english_cards %>% 
+  unnest(keywords,
+         keep_empty = T) %>%
+  group_by(id) %>%
+  summarise(keywords_pulled = paste0(keywords,
+                                     collapse = ','))
 
+english_cards$keywords = card_keywords$keywords_pulled
+
+card_games = english_cards %>%
+  unnest(games,
+         keep_empty = T) %>%
+  group_by(id) %>%
+  summarise(games_pulled = paste0(games,
+                                  collapse = ','))
+
+english_cards$games = card_games$games_pulled
+
+#Split the data into two files. One that contains core and expansion data, and one that contains all other release types
+english_core_expansion_cards = english_cards %>%
+  filter(set_type == 'expansion' | set_type == 'core')
+
+english_other_cards = english_cards %>%
+  filter(set_type != 'expansion' & set_type != 'core')
 
 # Write bulk card data to local drive
 fwrite(
-  english_cards, 
-  '~/R/Magic the Virgining/input/magic-default-cards-english.csv', 
+  english_core_expansion_cards, 
+  '~/R/Magic-the-Gathering/input/magic-default-cards-english.csv', 
   row.names = F
 )
 
-#pulling in data from wikipedia so we have a working list of sets that are either core or expansion sets (no commander only or unsets)
-url = 
-  'https://en.wikipedia.org/wiki/List_of_Magic:_The_Gathering_sets'
-
-# Get API call response
-response = 
-  GET(
-    url
-  )
-
-response = 
-  content(
-    response, 
-    'text'
-  )
-
-response = 
-  readHTMLTable(
-    response
-  )
-
-#table of core sets
-core_table = 
-  response[[1]]
-
-names(
-  core_table
-) = 
-  c(
-    'Set', 
-    'Set Symbol', 
-    'Set Code', 
-    'Pre-Release Date', 
-    'Release Date', 
-    'Size', 
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6'
-  )
-
-core_table = 
-  core_table[-1:-2, ]
-
-# table of expansion sets (no such thing as core sets after 2021 btw)
-expansion_table = 
-  response[[2]]
-
-names(expansion_table) = 
-  c(
-    'Set', 
-    'Set Symbol', 
-    'Set Code', 
-    'Pre-Release Date', 
-    'Release Date', 
-    'Size', 
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7'
-  )
-
-expansion_table = 
-  expansion_table[-1:-3, ]
-
-expansion_table = 
-  expansion_table %>%
-  filter(
-    !is.na(`Set Code`) & `Set Code` != 'unrevealed')
-
-expansion_table$`Set Code` = 
-  substr(expansion_table$`Set Code`,1,3)
-
-#making a master set list for reference
-master_set_list = 
-  append(
-    core_table$`Set Code`, 
-    expansion_table$`Set Code`
-  )
-
-#export master_set_list
-fwrite(
-  master_set_list, 
-  '~/R/Magic the Virgining/input/master-set-list.csv', 
-  row.names = F
-)
-
-#reducing english card dataset to only cards that are in the master set (no unsets or commander sets)
-core_and_expansion_cards = 
-  english_cards[english_cards$set %in% master_set_list, ]
-
-#create csv for core and expansion cards
-fwrite(
-  core_and_expansion_cards,
-  '~/R/Magic the Virgining/input/core-and-expansion-cards-english.csv',
-  row.names = F
-)
-
-#create text length column
-core_and_expansion_cards$text_length = 
-  nchar(
-    core_and_expansion_cards$oracle_text
-  )
-
-#create group by set/released at and average the text length
-core_and_expansion_cards_text_length_group = 
-  core_and_expansion_cards %>%
-  filter(
-    !is.na(text_length)
-  ) %>%
-  group_by(
-    set, 
-    rarity
-  ) %>%
-  summarise(
-    avg_text_length = 
-      mean(
-        text_length
-      ),
-    released_at = 
-      min(
-        released_at
-      )
-  )
-
-#combine set and release date for easy displaying in animated plot
-core_and_expansion_cards_text_length_group$released_at = 
-  paste(core_and_expansion_cards_text_length_group$released_at,
-        'Set:',
-        core_and_expansion_cards_text_length_group$set)
-
-#following code is the adjusted bar chart race code
-text_length_formatted <- core_and_expansion_cards_text_length_group %>%
-  group_by(released_at) %>% # The * 1 makes it possible to have non-integer ranks while sliding
-  mutate(rank = rank(-avg_text_length),
-         avg_text_length_rel = avg_text_length/avg_text_length[rank==1],
-         avg_text_length_lbl = paste0(" ",round(avg_text_length))) %>%
-  group_by(rarity) %>%
-  filter(rank <=10) %>%
-  ungroup()
-
-#create csv for animated bar chart
-fwrite(
-  text_length_formatted,
-  '~/R/Magic the Virgining/input/text-length-animation.csv',
-  row.names = F
-)
+fwrite(english_other_cards,
+       '~/R/Magic-the-Gathering/input/magic-not-core-or-expansion-cards.csv',
+       row.names = F)
